2a43eb84d115acc3d96570f2d89f75961fab2784
cocoliso <fuermiguel@yahoo.es>
2019-01-21 13:09:15 +0000

    Añadimos las funciones llamadas desde la rutas para homeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+     //Creamos las funciones llamadas desde las rutas
+     function getHome(){
+        return view('home');
+    }
 }
