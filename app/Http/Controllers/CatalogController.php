9a0be2f11418ca80062305b21ffc325522e3b52e
cocoliso <fuermiguel@yahoo.es>
2019-01-27 17:27:44 +0000

    Apartado seeders 01 completado

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -7,21 +7,7 @@ use Illuminate\Http\Request;
 class CatalogController extends Controller
 {
     //Definimos las variables
-    private $arrayClientes = array(
-        array(
-            'nombre' =>'Neo',
-            'imagen' =>'Imagen personalizada de la felicitación',
-            'fecha_nacimiento' => '06/01/1972',
-            'correo' => 'neo@matrix.org'
-            
-        ),
-        array(
-            'nombre' =>'Morfeo',
-            'imagen' =>'Imagen personalizada de la felicitación',
-            'fecha_nacimiento' =>'05/03/1997',
-            'correo' => 'morfeo@matrix.org'
-
-    ));
+  
     //Creamos las funciones llamadas desde las rutas
     function getIndex(){
         return view('/catalog/index')->with('arrayClientes',$this->arrayClientes);
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,9 +1,25 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Cliente;
 
 class DatabaseSeeder extends Seeder
 {
+    private $arrayClientes = array(
+        array(
+            'nombre' =>'Neo',
+            'imagen' =>'Imagen personalizada de la felicitación',
+            'fecha_nacimiento' => '06/01/1972',
+            'correo' => 'neo@matrix.org'
+            
+        ),
+        array(
+            'nombre' =>'Morfeo',
+            'imagen' =>'Imagen personalizada de la felicitación',
+            'fecha_nacimiento' =>'05/03/1997',
+            'correo' => 'morfeo@matrix.org'
+
+    ));
     /**
      * Seed the application's database.
      *
@@ -12,5 +28,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        self::seedCatalog(); //accediendo al método desde el interior de la definición de la clase
+        $this->command->info('Tabla cliente inicializda con datos!');
+    }
+
+    private function seedCatalog(){
+        DB::table('clientes')->delete();//Borrar el contenido de la tabla
+
+        foreach($this->arrayClientes as $cliente){
+            $c = new Cliente();
+            $c->nombre=$cliente['nombre'];
+            $c->imagen=$cliente['imagen'];
+            $c->fecha_nacimiento = $cliente['fecha_nacimiento'];
+            $c->correo=$cliente['correo'];
+            $c->save();
+        }
     }
 }
