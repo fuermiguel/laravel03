517aa47ccc5075406da72e8cadfe685706b2f69a
cocoliso <fuermiguel@yahoo.es>
2019-01-29 11:11:40 +0000

    Se finaliza el A41 añadiendo las consultas de eloquent y usando lo devuelto como objetos en las vistas

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Cliente;
 
+
 class CatalogController extends Controller
 {
     //Definimos las variables
@@ -12,23 +13,28 @@ class CatalogController extends Controller
   
     //Creamos las funciones llamadas desde las rutas
     function getIndex(){
-        //var_dump(Cliente::all());
-        return view('catalog.index')->with('arrayClientes',Cliente::all());
+        
+       return view('catalog.index')->with('arrayClientes',Cliente::all());
+       
    
     }
 
     function getShow($id){
-
-        return view('catalog/show')->with('cliente',$this->arrayClientes[$id]);
-
+        //!!!!!!!!!!!Tener cuidado con el id , porque las funciones del orm empiezan por uno
+        //Esto queda corregido al camniar el indice del array por el indice del propio objeto cliente
+        return view('catalog.show')->with('cliente',Cliente::findOrFail($id));
+        //El método findOrFail  es usado cuando queremos una exception si el modelo no es encontrado
     }
-
+    
     function getCreate(){
-
+        
         return view('catalog.create');
     }
-
+    
     function getEdit($id){
-        return view('catalog.edit')->with('id',$id);
+        //!!!!!!!!!!!Tener cuidado con el id , porque las funciones del orm empiezan por uno
+        //Esto queda corregido al camniar el indice del array por el indice del propio objeto cliente
+        return view('catalog.edit')->with('cliente',Cliente::findOrFail($id));
+        //El método findOrFail  es usado cuando queremos una exception si el modelo no es encontrado
     }
 }
