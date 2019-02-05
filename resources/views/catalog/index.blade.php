3b30da2a200996b426e7df4cf0cd4fc8e0841efe
cocoliso <fuermiguel@yahoo.es>
2019-02-05 11:05:44 +0000

    funciona el edit y borra imagen anterior cuando se edita, el bootstrap se me fue al carajo

--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -7,7 +7,7 @@
 @foreach( $arrayClientes as $key => $cliente )
 <div class="col-sm-4">
     <a href="{{ url('/catalog/show/' . $cliente->id ) }}">
-        <img src="{{$cliente->imagen}}" style="height:200px"/>  
+        <img src="{{asset($cliente->imagen)}}" style="height:200px"/>  
     </a>
     <h4 style="min-height:45px;margin:5px 0 10px 0">
             {{$cliente->nombre}}
